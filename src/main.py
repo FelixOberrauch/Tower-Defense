b83af82d3202e1c9614ee730261ee247e0a9a9fb
FelixOberrauch <felix.oberrauch@students.htl-ibk.at>
2026-04-14 10:45:11 +0200

    main updated

--- src/main.py
+++ src/main.py
@@ -1,4 +1,14 @@
 import pygame as pg
 from submodule import *
 
+# ____SETUP_____
 pg.init()
+HIGHT, WIDTH = 1920 ,1080
+screen = pg.display.set_mode((WIDTH, HIGHT))
+clock = pg.time.Clock()
+
+
+running = True
+while running:
+    pass
+    #___EVENTS___
